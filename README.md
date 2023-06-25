806e330ca6518725a7bcd72772a843551cbda6e3
impactaly <impactaky@users.noreply.github.com>
2023-06-25 12:17:42 +0900

    Refactoring

--- README.md
+++ README.md
@@ -7,13 +7,9 @@ A useful util library for writing shell script stream processing using
 
 ```typescript
 const stream = $`echo "abc\nabcde\nabcdef\nbcdefg"`
-  .map((l) => {
-    return `bug : ${l}`;
-  })
+  .map((l) => `bug : ${l}`)
   .$(`grep 'bug : a'`)
-  .filter((l) => {
-    return l.length > "bug : ".length + 5;
-  });
+  .filter((l) => l.length > "bug : ".length + 5);
 for await (const line of stream) {
   console.log(`de${line}`);
 }
--- command_builder.test.ts
+++ command_builder.test.ts
@@ -3,13 +3,9 @@ import { assertEquals } from "./test_deps.ts";
 
 Deno.test("Quick example", async () => {
   const stream = $`echo "abc\nabcde\nabcdef\nbcdefg"`
-    .map((l) => {
-      return `bug : ${l}`;
-    })
+    .map((l) => `bug : ${l}`)
     .$(`grep 'bug : a'`)
-    .filter((l) => {
-      return l.length > "bug : ".length + 5;
-    });
+    .filter((l) => l.length > "bug : ".length + 5);
   for await (const line of stream) {
     assertEquals(line, "bug : abcdef");
   }
