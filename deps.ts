248f1354cdc84cae2e8cc27ae6ff4ae1acba91f5
impactaly <impactaky@users.noreply.github.com>
2023-06-25 20:49:10 +0900

    Make xargs use spawn

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export { $, CommandBuilder } from "https://deno.land/x/dax@0.32.0/mod.ts";
+export { CommandChild } from "https://deno.land/x/dax@0.32.0/src/command.ts";
 export { TextLineStream } from "https://deno.land/std@0.182.0/streams/text_line_stream.ts";
