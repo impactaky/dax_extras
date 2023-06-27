c4ce305a58bd16f51dd303e1d0d973b419ab3e0b
Nbiba Bedis <bedisnbiba@gmail.com>
2023-06-27 21:24:10 +0100

    Refactor codebase
    (props to chatgpt for writing the docs)
    
    couple of notes/questions:
    
    - The old `pipe` method that was in CommandBuilder
    interface, seemd wrong: it was just being used internally
    as a helper, so I inlined it where its used, and replaced
    it with the appropriate code
    - stdout seems to be hardcoded in many places, can
    we do better ?
    - should CommandBuilder.prototype.pipe/$ wrap LineStream
    methods instead of having their own code (like the other functions)?

--- LineStream/LineStream.ts
+++ LineStream/LineStream.ts
@@ -28,15 +28,27 @@ class LineToByteStream extends TransformStream<string, Uint8Array> {
   }
 }
 
+/**
+ * Represents a stream of lines for reading the output of a command.
+ * It implements an async iterator, allowing iteration over the lines.
+ */
 export class LineStream {
-  stream: ReadableStream;
+  #stream: ReadableStream;
 
+  /**
+   * Constructs a new `LineStream` with the provided readable stream.
+   * @param stream - The readable stream to create the line stream from.
+   */
   constructor(stream: ReadableStream) {
-    this.stream = stream;
+    this.#stream = stream;
   }
 
+  /**
+   * Async iterator implementation for iterating over the lines of the stream.
+   * @returns An async iterator for iterating over the lines of the stream.
+   */
   async *[Symbol.asyncIterator]() {
-    const reader = this.stream.getReader();
+    const reader = this.#stream.getReader();
     while (true) {
       const { done, value } = await reader.read();
       if (done) break;
@@ -44,47 +56,87 @@ export class LineStream {
     }
   }
 
+  /**
+   * Reads the entire stream and returns the concatenated text.
+   * @returns A promise that resolves to the concatenated text.
+   */
   async text(): Promise<string> {
     let text = "";
-    for await (const line of this.stream) {
+    for await (const line of this.#stream) {
       text += line + "\n";
     }
     return text;
   }
 
+  /**
+   * Reads the stream and returns an array of lines.
+   * @returns A promise that resolves to an array of lines.
+   */
   async lines(): Promise<string[]> {
     const lines: string[] = [];
-    for await (const line of this.stream) {
+    for await (const line of this.#stream) {
       lines.push(line);
     }
     return lines;
   }
 
+  /**
+   * Pipes the stream through a transform stream.
+   * @param transform - The transform stream to pipe the line stream through.
+   * @returns A new line stream representing the piped stream.
+   */
   pipeThrough(transform: TransformStream): LineStream {
-    return new LineStream(this.stream.pipeThrough(transform));
+    return new LineStream(this.#stream.pipeThrough(transform));
   }
 
+  /**
+   * Pipes a command into the stdin of the next command in the chain.
+   * @param next - The command builder representing the next command.
+   * @returns The command builder with the stdin piped from the current stream.
+   */
   pipe(next: CommandBuilder): CommandBuilder {
-    const pipedStream = this.stream.pipeThrough(new LineToByteStream());
+    const pipedStream = this.#stream.pipeThrough(new LineToByteStream());
     return next.stdin(pipedStream);
   }
 
+  /**
+   * Pipes a command into the stdin of the next command in the chain.
+   * @param next - The command as a string to pipe into.
+   * @returns The command builder with the stdin piped from the current stream.
+   */
   $(next: string): CommandBuilder {
-    const pipedStream = this.stream.pipeThrough(new LineToByteStream());
+    const pipedStream = this.#stream.pipeThrough(new LineToByteStream());
     return new CommandBuilder().command(next).stdin(pipedStream);
   }
 
+  /**
+   * Maps the stream using a map function, allowing further processing.
+   * @param mapFunction - The function to map theoutput of each line.
+   *
+   * @param mapFunction - The function to map the output of each line.
+   * @returns A new line stream resulting from the mapping operation.
+   */
   map(mapFunction: MapFunction<string, string>): LineStream {
     return this.pipeThrough(new MapStream(mapFunction));
   }
 
+  /**
+   * Filters the stream using a filter function, allowing further processing.
+   * @param filterFunction - The function to filter the output of each line.
+   * @returns A new line stream resulting from the filtering operation.
+   */
   filter(filterFunction: FilterFunction<string>): LineStream {
     return this.pipeThrough(new FilterStream(filterFunction));
   }
 
+  /**
+   * Executes a command for each line of the stream using the provided xargs function.
+   * @param command - The xargs function that handles the execution of command lines.
+   * @returns A promise that resolves to an array of command builders representing the executed commands.
+   */
   async xargs(command: XargsFunction): Promise<CommandBuilder[]> {
     const processes: CommandBuilder[] = [];
-    for await (const line of this.stream) {
+    for await (const line of this.#stream) {
       processes.push(command(line));
     }
     return processes;
