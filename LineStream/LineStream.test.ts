b88930f0b47987a681326f4f5a637f55cfc83121
impactaly <impactaky@users.noreply.github.com>
2023-06-27 22:04:34 +0900

    Test LineStream.xargs

--- LineStream/LineStream.test.ts
+++ LineStream/LineStream.test.ts
@@ -40,3 +40,19 @@ Deno.test("LineStream.filter", async () => {
     .lines();
   assertEquals(text, ["4444"]);
 });
+
+Deno.test("LineStream.xargs one-line", async () => {
+  const result = await $`echo hello`
+    .lineStream()
+    .xargs((input) => $`echo ${input} world`)
+    .then((output) => output[0].xargs((input) => $`echo ${input} world2`));
+  assertEquals(await result[0].text(), "hello world world2");
+});
+
+Deno.test("LineStream.xargs multi-line", async () => {
+  const result = await $`echo "line1\nline2"`
+    .lineStream()
+    .xargs((input) => $`echo ${input} world`);
+  assertEquals(await result[0].text(), "line1 world");
+  assertEquals(await result[1].text(), "line2 world");
+});
