5f4a738d2e407e2f3056a17f79e72ebfe606aed6
impactaly <impactaky@users.noreply.github.com>
2023-07-03 23:49:39 +0900

    Update README

--- command_builder.test.ts
+++ command_builder.test.ts
@@ -2,14 +2,16 @@ import $ from "./mod.ts";
 import { assertEquals } from "./test_deps.ts";
 
 Deno.test("Quick example", async () => {
-  const stream = $`echo "abc\nabcde\nabcdef\nacddef\nbcdefg"`
-    .map((l) => `bug : ${l}`)
-    .$(`grep 'bug : a'`).noThrow()
-    .pipe($`grep 'bug : ab'`).noThrow()
-    .filter((l) => l.length > "bug : ".length + 5);
-  for await (const line of stream) {
-    assertEquals(line, "bug : abcdef");
-  }
+  const lines = await $`echo "olleh\nnop\ndlrow\nnop\nnop"` // => ["olleh", "nop", "dlrow", "nop", "nop"]
+    .$(`grep -v dummy`).noThrow() // => ["olleh", "nop", "dlrow", "nop", "nop"]
+    .apply((l) => {
+      if (l != "nop") {
+        return l.split("").reverse().join("");
+      }
+    }) // => ["hello", "world"]
+    .xargs((l) => $`echo ${l}!`)
+    .lines();
+  assertEquals(lines, ["hello!", "world!"]);
 });
 
 Deno.test("CommandBuilder.prototype.$", async () => {
