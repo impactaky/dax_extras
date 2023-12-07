2bffbfc7bfbefbf8f9cbefce9c818af4174e36f3
impactaky <37619203+impactaky@users.noreply.github.com>
2023-12-07 15:16:22 +0900

    Update dependencies (#32)
    Co-authored-by: impactaly <impactaky@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -3,8 +3,8 @@ export {
   CommandBuilder,
   CommandResult,
   PathRef,
-} from "https://deno.land/x/dax@0.33.0/mod.ts";
+} from "https://deno.land/x/dax@0.35.0/mod.ts";
 export {
   TextLineStream,
   toTransformStream,
-} from "https://deno.land/std@0.182.0/streams/mod.ts";
+} from "https://deno.land/std@0.208.0/streams/mod.ts";
