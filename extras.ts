66c2969574d4ceec1c03bd250370a6c4aebaf3a9
impactaly <impactaky@users.noreply.github.com>
2023-12-25 20:48:09 +0900

    Add $.split

--- extras.ts
+++ extras.ts
@@ -4,6 +4,9 @@ export const extras = {
   nproc(): number {
     return navigator.hardwareConcurrency;
   },
+  split(s: string) : string[] {
+    return s.trim().split(/\s+/);
+  },
   async xargs<T, R>(
     array: Iterable<T> | AsyncIterable<T>,
     iteratorFn: (data: T) => Promise<R>,
