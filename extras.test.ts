168a25b4077f73e4fe85abf9af8592900b7bf67b
impactaky <37619203+impactaky@users.noreply.github.com>
2023-12-25 20:43:34 +0900

    Add $.xargs (#40)
    Co-authored-by: impactaly <impactaky@users.noreply.github.com>

--- extras.test.ts
+++ extras.test.ts
@@ -4,3 +4,28 @@ import { assertEquals } from "./test_deps.ts";
 Deno.test("$.nproc", async () => {
   assertEquals($.nproc(), Number(await $`nproc`.text()));
 });
+
+Deno.test("$.xargs", async () => {
+  const func = async (n: number) => {
+    await $.sleep(n * 100);
+    return n * 2;
+  };
+  async function* generateNumbers() {
+    yield 3;
+    yield 2;
+    yield 2;
+  }
+  // should end in 5second
+  const resultPromise = $.xargs(generateNumbers(), func, 3);
+  let timerId: number | undefined;
+  const timeoutPromise = new Promise((_, reject) =>
+    timerId = setTimeout(() => reject(new Error("Timeout")), 400)
+  );
+  try {
+    await Promise.race([resultPromise, timeoutPromise]);
+  } finally {
+    if (timerId !== undefined) {
+      clearTimeout(timerId);
+    }
+  }
+});
